fc4f2e327b9af84330fdda39bf33b675177eaa9c
gaurav sharma <123371047+inggaurav@users.noreply.github.com>
2026-08-21 16:42:00 +0530

    Allow runtime auth repository to share Macro service configuration

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -3,5 +3,5 @@ import '../core/auth/auth_repository.dart';
 export '../core/auth/auth_repository.dart';
 
 class AuthRepository extends AuthRepositoryImpl {
-  AuthRepository({super.storage});
+  AuthRepository({super.storage, super.config});
 }
